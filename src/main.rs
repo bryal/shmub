7f9a0e451c6640300b117d489971ae6d8980b7a3
bryal <96.bryal@gmail.com>
2018-08-05 17:37:52 +0200

    Receive UDP packets of `shmub_common::Packet` from client and write .wav file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate hound;
+extern crate shmub_common;
+
+use shmub_common::*;
+use std::net::{Ipv4Addr, UdpSocket};
+
+const PORT: u16 = 14320;
+
 fn main() {
-    
+    let socket = UdpSocket::bind((Ipv4Addr::new(0, 0, 0, 0), PORT)).expect("Failed to open socket");
+    let mut buf = [0; PACKET_SIZE];
+    let mut packets = Vec::new();
+    for _ in 0..1000 {
+        socket
+            .recv_from(&mut buf[..])
+            .expect("Error receiving from socket");
+        packets.push(Packet::parse(&buf[..]).expect("Error parsing packet"));
+    }
+    let pcm_samples = packets
+        .into_iter()
+        .flat_map(|p| p.samples()[..].iter().cloned().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 96000,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create("recording.wav", spec).unwrap();
+    for sample in pcm_samples {
+        writer.write_sample(sample).unwrap();
+    }
+    writer.finalize().unwrap();
 }
