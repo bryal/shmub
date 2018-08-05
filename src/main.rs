d46b7f728c35d6b9b76ec1c0a74c5c637ebfb79d
bryal <96.bryal@gmail.com>
2018-08-06 00:28:51 +0200

    Receive/write stereo data

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let socket = UdpSocket::bind((Ipv4Addr::new(0, 0, 0, 0), PORT)).expect("Failed to open socket");
     let mut buf = [0; PACKET_SIZE];
     let mut packets = Vec::new();
-    for _ in 0..1000 {
+    for _ in 0..4000 {
         socket
             .recv_from(&mut buf[..])
             .expect("Error receiving from socket");
@@ -21,14 +21,15 @@ fn main() {
         .flat_map(|p| p.samples()[..].iter().cloned().collect::<Vec<_>>())
         .collect::<Vec<_>>();
     let spec = hound::WavSpec {
-        channels: 1,
-        sample_rate: 96000,
+        channels: N_CHANNELS as u16,
+        sample_rate: SAMPLE_RATE,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
     let mut writer = hound::WavWriter::create("recording.wav", spec).unwrap();
     for sample in pcm_samples {
-        writer.write_sample(sample).unwrap();
+        writer.write_sample(sample[0]).unwrap();
+        writer.write_sample(sample[1]).unwrap();
     }
     writer.finalize().unwrap();
 }
