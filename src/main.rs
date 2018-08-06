fe9f466061b86e51623f53ceb2fed3358794d21d
bryal <96.bryal@gmail.com>
2018-08-06 02:35:46 +0200

    Write samples of received packets to output via synced channel

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,97 @@
-extern crate hound;
+extern crate cpal;
 extern crate shmub_common;
 
+use cpal::{Format, OutputBuffer, SampleFormat, SampleRate, StreamData, UnknownTypeOutputBuffer};
 use shmub_common::*;
 use std::net::{Ipv4Addr, UdpSocket};
+use std::sync::mpsc;
+use std::thread;
 
 const PORT: u16 = 14320;
+const CHANNEL_N_BUFFERED_SAMPLES: usize = 1024;
 
 fn main() {
-    let socket = UdpSocket::bind((Ipv4Addr::new(0, 0, 0, 0), PORT)).expect("Failed to open socket");
-    let mut buf = [0; PACKET_SIZE];
-    let mut packets = Vec::new();
-    for _ in 0..4000 {
-        socket
-            .recv_from(&mut buf[..])
-            .expect("Error receiving from socket");
-        packets.push(Packet::parse(&buf[..]).expect("Error parsing packet"));
-    }
-    let pcm_samples = packets
-        .into_iter()
-        .flat_map(|p| p.samples()[..].iter().cloned().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    let spec = hound::WavSpec {
+    println!("Shmub Audio Server");
+    println!("Receive streamed UDP audio data and output to connected sound device.");
+    println!("");
+    let device = prompt_device();
+    let format = Format {
         channels: N_CHANNELS as u16,
-        sample_rate: SAMPLE_RATE,
-        bits_per_sample: 16,
-        sample_format: hound::SampleFormat::Int,
+        sample_rate: SampleRate(SAMPLE_RATE),
+        data_type: SampleFormat::I16,
     };
-    let mut writer = hound::WavWriter::create("recording.wav", spec).unwrap();
-    for sample in pcm_samples {
-        writer.write_sample(sample[0]).unwrap();
-        writer.write_sample(sample[1]).unwrap();
+    let event_loop = cpal::EventLoop::new();
+    let stream_id = event_loop
+        .build_output_stream(&device, &format)
+        .expect(&format!(
+            "Failed to build output stream for device {} with format {:?}",
+            device.name(),
+            format
+        ));
+    event_loop.play_stream(stream_id);
+    let socket = UdpSocket::bind((Ipv4Addr::new(0, 0, 0, 0), PORT)).expect("Failed to open socket");
+    // Sample channel
+    let (tx, rx) = mpsc::sync_channel::<[i16; N_CHANNELS]>(CHANNEL_N_BUFFERED_SAMPLES);
+    thread::spawn(move || {
+        event_loop.run(move |_, data| {
+            let mut out_buf = get_i16_buffer(as_output_buffer(data));
+            for out_sample in out_buf.chunks_mut(N_CHANNELS) {
+                let sample = rx.recv().expect("Error receiving sample");
+                for (out, &value) in out_sample.iter_mut().zip(&sample[..]) {
+                    *out = value;
+                }
+            }
+        });
+    });
+    loop {
+        let packet = recv_packet(&socket);
+        for sample in packet.samples()[..].iter().cloned() {
+            match tx.try_send(sample) {
+                Ok(_) | Err(mpsc::TrySendError::Full(_)) => (),
+                Err(mpsc::TrySendError::Disconnected(_)) => {
+                    panic!("Lost connection to output thread")
+                }
+            }
+        }
     }
-    writer.finalize().unwrap();
+}
+
+fn get_i16_buffer<'b>(unknown_buf: UnknownTypeOutputBuffer<'b>) -> OutputBuffer<'b, i16> {
+    match unknown_buf {
+        UnknownTypeOutputBuffer::I16(buf) => buf,
+        _ => panic!("Expected I16 buffer"),
+    }
+}
+
+fn as_output_buffer(data: StreamData) -> UnknownTypeOutputBuffer {
+    match data {
+        StreamData::Output { buffer } => buffer,
+        _ => panic!("Expected output data buffer"),
+    }
+}
+
+fn recv_packet(socket: &UdpSocket) -> Packet {
+    let mut buf = [0; PACKET_SIZE];
+    socket
+        .recv_from(&mut buf[..])
+        .expect("Error receiving from socket");
+    Packet::parse(&buf[..]).expect("Error parsing packet")
+}
+
+fn prompt_device() -> cpal::Device {
+    println!("Select audio output device:");
+    let mut devices = cpal::output_devices().collect::<Vec<_>>();
+    for (i, device) in devices.iter().enumerate() {
+        println!(" {}) {}", i, device.name());
+    }
+    let line = prompt_line();
+    let i = line.parse::<usize>().expect(&format!(
+        "Failed to parse input as device index: line = \"{}\"",
+        line
+    ));
+    if i >= devices.len() {
+        panic!("Undefined device index");
+    } else {
+        devices.remove(i)
+    }
 }
