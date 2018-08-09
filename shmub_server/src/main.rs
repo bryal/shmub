46980e56f099860400e86e776e17a3ebb11e984f
Johan Johansson <96.bryal@gmail.com>
2018-08-09 15:16:07 +0200

    `AudioBuffer` for proper buffering in server

--- shmub_server/src/main.rs
+++ shmub_server/src/main.rs
@@ -1,14 +1,17 @@
 extern crate cpal;
 extern crate shmub_common;
+extern crate std_semaphore;
 
 use cpal::{Format, OutputBuffer, SampleFormat, SampleRate, StreamData, UnknownTypeOutputBuffer};
 use shmub_common::*;
+use std::collections::VecDeque;
 use std::net::{Ipv4Addr, UdpSocket};
-use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
+use std_semaphore::Semaphore;
 
 const PORT: u16 = 14320;
-const CHANNEL_N_BUFFERED_SAMPLES: usize = 1024;
+const N_BUFFERED_SAMPLES: usize = 1024;
 
 fn main() {
     println!("Shmub Audio Server");
@@ -30,29 +33,91 @@ fn main() {
         ));
     event_loop.play_stream(stream_id);
     let socket = UdpSocket::bind((Ipv4Addr::new(0, 0, 0, 0), PORT)).expect("Failed to open socket");
+
     // Sample channel
-    let (tx, rx) = mpsc::sync_channel::<[i16; N_CHANNELS]>(CHANNEL_N_BUFFERED_SAMPLES);
+    let buffer = AudioBuffer::new(N_BUFFERED_SAMPLES);
+    let buffer2 = buffer.clone();
+
     thread::spawn(move || {
         event_loop.run(move |_, data| {
             let mut out_buf = get_i16_buffer(as_output_buffer(data));
             for out_sample in out_buf.chunks_mut(N_CHANNELS) {
-                let sample = rx.recv().expect("Error receiving sample");
+                let sample = buffer2.pop_front();
                 for (out, &value) in out_sample.iter_mut().zip(&sample[..]) {
                     *out = value;
                 }
             }
         });
     });
+
     loop {
         let packet = recv_packet(&socket);
         for sample in packet.samples()[..].iter().cloned() {
-            match tx.try_send(sample) {
-                Ok(_) | Err(mpsc::TrySendError::Full(_)) => (),
-                Err(mpsc::TrySendError::Disconnected(_)) => {
-                    panic!("Lost connection to output thread")
-                }
-            }
+            buffer.try_push_back(sample);
         }
+    }
+}
+
+/// A thread-safe buffer of samples that handles buffering in a way that makes sense for audio
+#[derive(Clone)]
+struct AudioBuffer {
+    guarded_queue: Arc<Mutex<VecDeque<Sample>>>,
+    optimal_size: usize,
+    available_samples: Arc<Semaphore>,
+}
+
+impl AudioBuffer {
+    fn new(optimal_size: usize) -> Self {
+        assert!(optimal_size > 1, "Optimal size must be greater than 1");
+        AudioBuffer {
+            guarded_queue: Arc::new(Mutex::new(VecDeque::with_capacity(optimal_size * 2))),
+            optimal_size,
+            available_samples: Arc::new(Semaphore::new(0)),
+        }
+    }
+
+    /// If the buffer is not unbuffering to `optimal_size`, push the sample
+    ///
+    /// Returns whether the sample was pushed
+    fn try_push_back(&self, sample: Sample) -> bool {
+        let mut queue = self
+            .guarded_queue
+            .lock()
+            .expect("Error locking queue mutex");
+        if queue.len() < self.optimal_size * 2 {
+            queue.push_back(sample);
+            self.available_samples.release();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn wait_for_buffering(&self) {
+        for _ in 0..self.optimal_size {
+            self.available_samples.acquire();
+        }
+        for _ in 0..self.optimal_size {
+            self.available_samples.release();
+        }
+    }
+
+    /// If the buffer is not buffering to `optimal_size`, pop a sample
+    fn pop_front(&self) -> Sample {
+        self.available_samples.acquire();
+        let (sample, wait) = {
+            let mut queue = self
+                .guarded_queue
+                .lock()
+                .expect("error locking queue mutex");
+            let sample = queue.pop_front().expect("no element to pop queue");
+            let wait = queue.len() == 1;
+            (sample, wait)
+        };
+        if wait {
+            self.wait_for_buffering()
+        }
+        sample
     }
 }
 
